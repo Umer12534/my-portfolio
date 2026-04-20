c5f980c008f40118c8829710caad9b23c7ce2d98
Syed Umer <133738875+Umer12534@users.noreply.github.com>
2026-04-20 22:24:07 +0500

    add home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import Navbar from '../components/navbar/Navbar'
+import Hero from '../components/heloSection/Herosection'
+import ProfileSection from '../components/Profile/Profile'
+import WhatImDoing from '../components/Whatimdoing/Whatimdoing'
 
 const Home = () => {
   return (
-    <div>
+    <div className="app-shell">
       <Navbar />
+      <Hero />
+      <ProfileSection />
+      <WhatImDoing />
+      
+      <div style={{ height: '200vh' }}></div>
     </div>
   )
 }
